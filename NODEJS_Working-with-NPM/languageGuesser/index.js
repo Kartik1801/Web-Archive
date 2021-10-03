52321817b7d4ec07e11ac3a6c2358731ed602e3a
kartik1801 <kartikdhawan1801@gmail.com>
2021-10-03 22:37:46 +0530

    Added NodeJS_Working-with-NPM_Language-Guesser

---
+++ NODEJS_Working-with-NPM/languageGuesser/index.js
@@ -0,0 +1,11 @@
+// Language Guess
+const args=process.argv[2];
+const franc=require('franc');
+const lang = require('langs')   
+element=args
+const code=franc(element);
+
+if(code!='und')
+    console.log(lang.where('3',code).name);
+else
+    console.log('Could not Find a proper match. Try Another Sample');        

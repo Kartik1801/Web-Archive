1aa12033c22d682c52527f0aa51aeed93fd766ad
kartik1801 <kartikdhawan1801@gmail.com>
2021-10-04 23:37:14 +0530

    Added EJS_Templating-Demo

---
+++ EXPRESS/EJS/Templating Demo/index.js
@@ -0,0 +1,54 @@
+const express =require('express');
+const app =express();
+let port = process.argv[2];
+if(!port){
+    port=3000;
+}
+const path = require('path');
+/* 
+ EJS is automatically required by express behind the scenes.
+ 
+ By default, When we us a view engine, express is going to assume that all our views/templates
+ exist in a directory "/views" (or) We can select a directory using views property of app.set()
+
+ NOTE : If we run this file in some different directory then it will generate an error when
+        as by default when we use an view engine, express just append process.cwd() with views
+        Instead we can use path module to set the directory of views folder ourself 
+*/
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+
+app.get('/', (req, res) =>{
+    res.render('home');
+})
+
+app.get('/r/:subreddit',(req,res)=>{
+    const {subreddit} = req.params;
+    console.log(subreddit);
+    res.render('subreddit',{subreddit:subreddit})
+})
+
+app.get("/eo",(req,res)=>{
+    const {num} = req.query
+    res.render('evenodd',{number: num});
+})
+
+app.get("/diceroll",(req,res)=>{
+    num = Math.floor(Math.random()*6+1);
+    res.render('random',{diceroll: num});
+})
+
+app.get("/cats",(req,res)=>{
+const cats=[
+    "Blue","Roxy",'Neko-chan',"Nyan Pasu"
+]
+res.render('cats',{ cats : cats})
+})
+
+app.get('*',(req,res)=>{
+    res.render("404")
+})
+
+app.listen(port, () =>{
+    console.log(`listening on port ${port}`);
+})

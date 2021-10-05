d40915d5ece03c54cf576e8d06191b7e0ba1b3a0
kartik1801 <kartikdhawan1801@gmail.com>
2021-10-05 22:07:02 +0530

    updated

--- EXPRESS/firstapp/index.js
+++ EXPRESS/firstapp/index.js
@@ -5,7 +5,7 @@ const port=process.argv[2]
 if(port)
 {
     /* //Every time a request is made use function is executed    
-    app.use((req,res)=>{
+        app.use((req,res)=>{
         console.log('We got your request');
         //res.send({'Hello, We got your request'});  
         //res.send({'color': 'green', 'name':'Xmen'});  

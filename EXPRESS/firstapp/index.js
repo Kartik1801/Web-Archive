9edcafb85aeccecea4d2c4e740dea8f0be77d166
kartik1801 <kartikdhawan1801@gmail.com>
2021-10-05 22:08:08 +0530

    updated

--- EXPRESS/firstapp/index.js
+++ EXPRESS/firstapp/index.js
@@ -4,13 +4,13 @@ const app=exp();
 const port=process.argv[2]
 if(port)
 {
-    /* //Every time a request is made use function is executed    
-        app.use((req,res)=>{
-        console.log('We got your request');
-        //res.send({'Hello, We got your request'});  
-        //res.send({'color': 'green', 'name':'Xmen'});  
-        res.send('<h1>Hello, We got your request</h1>');   
-    })
+    /* // Every time a request is made use function is executed    
+            app.use((req,res)=>{
+                console.log('We got your request');
+                //  res.send({'Hello, We got your request'});  
+                //  res.send({'color': 'green', 'name':'Xmen'});  
+                res.send('<h1>Hello, We got your request</h1>');   
+            })
  */
 
     app.get("/r/:subreddit",(req,res) =>{

3703a7b3524b6ef87eb17edd5b358df34c40dd94
Kartik1801 <kartikdhawan1801@gmail.com>
2021-10-08 23:39:37 +0530

    initial commit

---
+++ EXPRESS_Restful-Routes/RESTDEMO/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+let port = process.argv[2]; 
+if(!port){
+    port=3000
+}
+
+app.get('/tacos',(req,res)=>{
+    res.send("GET /Tacos Response !");
+})
+app.listen(port,()=>{
+    console.log("Listening on port: " + port);
+})

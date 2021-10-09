c539fe3fd66b0ebc8001023258380900a4ae766f
Kartik1801 <kartikdhawan1801@gmail.com>
2021-10-09 23:23:09 +0530

    updated

--- EXPRESS_Restful-Routes/BASICS/index.js
+++ EXPRESS_Restful-Routes/BASICS/index.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const app = express();
+
 let port = process.argv[2]; 
+
 if(!port){
     port=3000
 }
 
 // app.use is used to run some function on every single route/ request.
 // express.json(), express.urlencoded() are middleware used to parse the request body that is coming along with the post request.
+
 app.use(express.urlencoded({extended:true}));
+
 app.use(express.json());
 
 app.get('/tacos',(req,res)=>{

4118156cb8675ab6e428dc1f36a7d3750b47bda2
Kartik1801 <kartikdhawan1801@gmail.com>
2021-10-11 23:09:31 +0530

    updated

--- EXPRESS_Restful-Routes/RESTDEMO/index.js
+++ EXPRESS_Restful-Routes/RESTDEMO/index.js
@@ -1,18 +1,24 @@
+// RESTFUL ROUTES DEMO
 const express = require("express");
 const app = express();
 
 let port = process.argv[2]; 
+
 if(!port){
     port=3000
 }
 /* 
+
 RESTFUL ROUTES using comments example: 
+
     GET /comments - list all comments.                      - Index Route.
     POST /comments - create a new comment.                  - Create Route. 
     GET /comments/:id - Get one comment (using id)          - Show Route.88
     PUT or PATCH /comments/:id - updates one comment.       - Update Route.
     DELETE /comments/:id - Deletes one comment.             - Delete / Destroy Route.
+
 */
+
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
@@ -20,6 +26,7 @@ app.get('/tacos',(req,res)=>{
     console.log(req.query);
     res.send("GET /Tacos Response !");
 })
+
 app.post('/tacos',(req,res)=>{
     console.log(req.body);
     res.send("POST /Tacos Response !");

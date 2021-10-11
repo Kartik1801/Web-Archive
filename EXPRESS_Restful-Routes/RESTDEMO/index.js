fd6b16c903c73424667056b3a2ee95c997f4de94
Kartik1801 <kartikdhawan1801@gmail.com>
2021-10-11 23:15:32 +0530

    updated

--- EXPRESS_Restful-Routes/RESTDEMO/index.js
+++ EXPRESS_Restful-Routes/RESTDEMO/index.js
@@ -11,26 +11,20 @@ RESTFUL ROUTES using comments example:
 
 const express = require("express");
 const app = express();
-
 let port = process.argv[2]; 
-
 if(!port){
     port=3000
 }
-
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-
 app.get('/tacos',(req,res)=>{
     console.log(req.query);
     res.send("GET /Tacos Response !");
 })
-
 app.post('/tacos',(req,res)=>{
     console.log(req.body);
     res.send("POST /Tacos Response !");
 })
-
 app.listen(port,()=>{
     console.log("Listening on port: " + port);
 })

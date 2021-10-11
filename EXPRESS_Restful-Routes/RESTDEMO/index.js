1b27d1a95d796f8089e9a7b04616b818687eeb52
Kartik1801 <kartikdhawan1801@gmail.com>
2021-10-11 23:08:05 +0530

    updated

--- EXPRESS_Restful-Routes/RESTDEMO/index.js
+++ EXPRESS_Restful-Routes/RESTDEMO/index.js
@@ -7,10 +7,9 @@ if(!port){
 }
 /* 
 RESTFUL ROUTES using comments example: 
-
     GET /comments - list all comments.                      - Index Route.
     POST /comments - create a new comment.                  - Create Route. 
-    GET /comments/:id - Get one comment (using id)          - Show Route.
+    GET /comments/:id - Get one comment (using id)          - Show Route.88
     PUT or PATCH /comments/:id - updates one comment.       - Update Route.
     DELETE /comments/:id - Deletes one comment.             - Delete / Destroy Route.
 */

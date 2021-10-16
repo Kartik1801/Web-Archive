6d935aad21631e88c7825c494bffdef6f34443d8
Kartik1801 <kartikdhawan1801@gmail.com>
2021-10-17 01:13:58 +0530

    updated

--- EXPRESS_Restful-Routes/RESTDEMO/index.js
+++ EXPRESS_Restful-Routes/RESTDEMO/index.js
@@ -2,37 +2,62 @@ const express = require("express");
 const app = express();
 let port = process.argv[2]; 
 const path=require('path');
+const { ppid } = require("process");
 if(!port){
     port=3000
 }
 
 const comments=[
     {
+        id: 1,
         username: "Todd",
         comment: "LOL"
     },
     {
+        id: 2,
         username: "Sammy",
         comment: "I am watching bird with ................"
     },
-    {
+    {   
+        id: 3,
         username: "JOJO",
         comment: "I dont understand whats going on"
     },
     {
+        id: 4,
         username: "Yuta",
         comment: "Konichiwa"
     }
 ]
+
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+
 app.set("views",path.join(__dirname,"views"))
 app.set("view engine", "ejs");
 
 
 app.get('/comments',(req,res)=>{
-    res.render('comments/index');    
+    res.render('comments/index',{comments});    
 })
+
+app.get('/comments/new',(req,res)=>{
+    res.render('comments/new');
+})
+
+app.get('/comments/:id',(req,res)=>{
+    const {id}=req.params;
+    const comment= comments.find(c=>c.id===parseInt(id))
+    res.render('comments/show',{comment});
+})
+
+app.post('/comments',(req,res)=>{
+    const {username,comment}=req.body;
+    comments.push({username,comment});    
+    console.log(req.body);
+    res.redirect('/comments');
+});
+
 
 app.get('/tacos',(req,res)=>{
     console.log(req.query);

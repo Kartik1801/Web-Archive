c936e460a180057e4debb820d96b10743d790a88
Kartik1801 <kartikdhawan1801@gmail.com>
2021-10-16 23:50:58 +0530

    updated

--- EXPRESS_Restful-Routes/RESTDEMO/index.js
+++ EXPRESS_Restful-Routes/RESTDEMO/index.js
@@ -1,21 +1,39 @@
-/* 
-RESTFUL ROUTES using comments example: 
-
-    GET /comments - List/show all comments.                  - Index Route.
-    POST /comments - Creates a new comment.                  - Create Route. 
-    GET /comments/:id - Get one comment (using id)           - Show Route.
-    PUT or PATCH /comments/:id - Updates one comment.        - Update Route.
-    DELETE /comments/:id - Deletes one comment.              - Delete/Destroy Route.
-
-*/
 const express = require("express");
 const app = express();
 let port = process.argv[2]; 
+const path=require('path');
 if(!port){
     port=3000
 }
+
+const comments=[
+    {
+        username: "Todd",
+        comment: "LOL"
+    },
+    {
+        username: "Sammy",
+        comment: "I am watching bird with ................"
+    },
+    {
+        username: "JOJO",
+        comment: "I dont understand whats going on"
+    },
+    {
+        username: "Yuta",
+        comment: "Konichiwa"
+    }
+]
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.set("views",path.join(__dirname,"views"))
+app.set("view engine", "ejs");
+
+
+app.get('/comments',(req,res)=>{
+    res.render('comments/index');    
+})
+
 app.get('/tacos',(req,res)=>{
     console.log(req.query);
     res.send("GET /Tacos Response !");
@@ -27,3 +45,13 @@ app.post('/tacos',(req,res)=>{
 app.listen(port,()=>{
     console.log("Listening on port: " + port);
 })
+/* 
+RESTFUL ROUTES using comments example: 
+
+    GET /comments - List/show all comments.                  - Index Route.
+    POST /comments - Creates a new comment.                  - Create Route. 
+    GET /comments/:id - Get one comment (using id)           - Show Route.
+    PUT or PATCH /comments/:id - Updates one comment.        - Update Route.
+    DELETE /comments/:id - Deletes one comment.              - Delete/Destroy Route.
+
+*/

575201f3cdbf1f534a13e69a5140c0dfbc32df19
Kartik1801 <kartikdhawan1801@gmail.com>
2021-10-28 21:37:57 +0530

    update

--- NODEJS_Basics/boilerplate.js
+++ NODEJS_Basics/boilerplate.js
@@ -3,15 +3,15 @@ const fs = require('fs');
 const path = require('path')
 const folderName= process.argv.slice(2);
 if(folderName[0]){
- folderName.forEach(element =>{
- try{
-    fs.mkdirSync(element);
-    fs.mkdirSync(`${path.join(element)}/Assets`);
-    fs.writeFileSync(`${path.join(element,'index.html')}`,'');
-    fs.writeFileSync(`${path.join(element,'/Assets/app.css')}`,''); 
-    fs.writeFileSync(`${path.join(element,'/Assets/app.js')}`,''); 
-    console.log(`Created Boilerplate at ${element}`)
- }
+   folderName.forEach(element=>{
+      try{
+         fs.mkdirSync(element);
+         fs.mkdirSync(`${path.join(element)}/Assets`);
+         fs.writeFileSync(`${path.join(element,'index.html')}`,'');
+         fs.writeFileSync(`${path.join(element,'/Assets/app.css')}`,''); 
+         fs.writeFileSync(`${path.join(element,'/Assets/app.js')}`,''); 
+         console.log(`Created Boilerplate at ${element}`)
+      }
  catch(e){
     console.log("Something is wrong!");
     console.log(e);

6901c64ae97fcd7a9c9d20fce15fb32f3e41f5af
Kartik1801 <kartikdhawan1801@gmail.com>
2021-10-29 21:28:25 +0530

    update

--- EXPRESS_Restful-Routes/BASICS/index.js
+++ EXPRESS_Restful-Routes/BASICS/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 let port = process.argv[2]; 
 if(!port){
-    port=3000
+    port=3000;
 }
 // app.use is used to run some function on every single route/ request.
 // express.json(), express.urlencoded() are middleware used to parse the request body that is coming along with the post request.
@@ -11,11 +11,11 @@ app.use(express.json());
 app.get('/tacos',(req,res)=>{
     console.log(req.query);
     res.send("GET /Tacos Response !");
-})
+});
 app.post('/tacos',(req,res)=>{
     console.log(req.body);
     res.send("POST /Tacos Response !");
-})
+});
 app.listen(port,()=>{
     console.log("Listening on port: " + port);
-})
+});

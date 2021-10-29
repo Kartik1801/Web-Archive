7e08f1c04475451fe3b066b4e081cb3f2bcf0537
Kartik1801 <kartikdhawan1801@gmail.com>
2021-10-29 21:28:39 +0530

    update

--- NODEJS_Basics/args.js
+++ NODEJS_Basics/args.js
@@ -1,7 +1,7 @@
-console.log('Hello Args')
+console.log('Hello Args');
 // process.argv returns an array of
 // 1. path where node is installed at index 0
 // 2. path where our file that is being run is present at index 1  
 // 3 space seprated command line arguments from index 2 to ...
 // (ie node args.js a b c -- returns ["path","path where args.js is present"],'a','b','c').
-console.log(process.argv)
+console.log(process.argv);

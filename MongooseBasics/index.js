2eb4069cda3d8bf443084c4bb367fcd7984e54ca
Kartik1801 <kartikdhawan1801@gmail.com>
2021-11-03 23:09:33 +0530

    update

---
+++ MongooseBasics/index.js
@@ -0,0 +1,9 @@
+// const {TextDecoder, TextEncoder} = require("util");
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/movieApp')
+.then(()=>{console.log("Connected!!!");}
+    
+)
+.catch((err)=>{
+    console.log("Error",err);
+})

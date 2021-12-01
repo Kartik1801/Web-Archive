b71dfab3437b5557cfba5f5e10a0deab9efd0583
kartik1801 <kartikdhawan1801@gmail.com>
2021-12-01 23:43:59 +0530

    updated

--- MongooseBasics/index.js
+++ MongooseBasics/index.js
@@ -1,9 +1,17 @@
 // const {TextDecoder, TextEncoder} = require("util");
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/movieApp')
-.then(()=>{console.log("Connected!!!");}
-    
+.then(()=>{console.log("Connected!!!");}  
 )
 .catch((err)=>{
     console.log("Error",err);
 })
+const movieSchema = new mongoose.Schema({
+    title: String,
+    year: Number,
+    score: Number,
+    rating: String,
+});
+const Movie = mongoose.model('Movie',movieSchema);
+const VE = new Movie({title: "Violet Evergarden", year: 2021,rating: "U",score: 9});
+VE.save();

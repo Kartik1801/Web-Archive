bdaa4051ed5909f80997c8b36b48a06d3052b175
kartik1801 <kartikdhawan1801@gmail.com>
2021-12-06 19:58:37 +0530

    updated

--- MongooseBasics/index.js
+++ MongooseBasics/index.js
@@ -17,6 +17,7 @@ const VE = new Movie({ title: "Violet Evergarden", year: 2021, rating: "U", scor
 // use VE.save() to send to database
 
 /* 
+
 Movie.insertMany([
     {title: "The Silent Voice", year: 2019, rating: "U", score: 8.5},
     {title: "Weathering with You", year: 2019, rating: "U", score: 9.0},
@@ -32,20 +33,17 @@ Movie.insertMany([
 */
 
 /* 
-Movie.find({})
-.then((x) =>{console.log(x)})
-Movie.find({year:{$lt: 2021}}).then(data => data.forEach((d)=>console.log(d.title)))
-*/
 
+Movie.find({}).then((x) =>{console.log(x)})
+Movie.find({year:{$lt: 2021}}).then(data => data.forEach((d)=>console.log(d.title)))
+
+*/
 /* 
+
 Movie.updateOne({title: "Violet Evergarden: The Movie"},{score: 8.98})
 .then((result) => {console.log("Updated",result)});
-*/
-/* 
 Movie.updateMany({ title: {$in : ["The Silent Voice", "Weathering with You","I want to eat your pancreas","Colorful"]}},{rating: "PG-13"})
 .then(res => console.log("Updated",res)) 
-*/
-/* 
 Movie.findOneAndUpdate({ title:"Colorful"}, {year: 2010, score: 7.79},{new:true})
 .then((res=> console.log("Updated",res)))
 Movie.findOneAndUpdate({ title:"The Silent Voice"}, {altname:"Koe no Katachi", year: 2016, score: 8.97},{new:true})
@@ -54,4 +52,11 @@ Movie.findOneAndUpdate({ title:"I want to eat your pancreas",}, { altname:"Kimi 
 .then((res=> console.log("Updated",res)))
 Movie.findOneAndUpdate({ title:"Weathering with You"}, { altname: "Tenki no Ko", year: 2019, score: 8.34},{new:true})
 .then((res=> console.log("Updated",res)))
+
+*/
+
+/* 
+
+DEPRECATED ~ Movie.remove({ title:"Colorful"}).then((r)=>console.log("",r)) ~USE ~ 
+
 */

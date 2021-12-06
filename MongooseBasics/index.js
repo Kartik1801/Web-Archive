77e7d1012e30489cce1a2d5ca295f450e0e31492
kartik1801 <kartikdhawan1801@gmail.com>
2021-12-06 18:28:02 +0530

    updated

--- MongooseBasics/index.js
+++ MongooseBasics/index.js
@@ -14,16 +14,20 @@ const movieSchema = new mongoose.Schema({
 const Movie = mongoose.model('Movie',movieSchema);
 const VE = new Movie({ title: "Violet Evergarden", year: 2021, rating: "U", score: 9 });
 // use VE.save() to send to database
-/* Movie.insertMany([
+/* 
+Movie.insertMany([
     { title: "The Silent Voice", year: 2019, rating: "U", score: 8.5 },
     { title: "Weathering with You", year: 2019, rating: "U", score: 9.0 },
     { title: "I want to eat your pancreas", year: 2018, rating: "U", score: 9.2 },
     { title: "Colorful", year: 2021, rating: "U", score: 10 }
-]); */
+]);
+ */
 /* 
 Movie.find({})
 .then((x) =>{console.log(x)})
 Movie.find({year:{$lt: 2021}}).then(data => data.forEach((d)=>console.log(d.title)))
  */
+/* 
 Movie.updateOne({title: "Violet Evergarden"},{title: "Violet Evergarden: The Movie", rating: "PG-13",year: 2020})
 .then(() => {console.log("Updated")});
+ */

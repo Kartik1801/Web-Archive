de2fc59a1602781087e2c3b77e47af5e5cb2053a
kartik1801 <kartikdhawan1801@gmail.com>
2021-12-11 23:57:57 +0530

    EXPRESS + MONGOOSE

---
+++ EXPRESS_MONGOOSE/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const mongoose = require('mongoose');
+require('dotenv').config
+
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+app.listen(process.env.PORT, ()=>{
+    console.log('listening on port: ',process.env.PORT);
+})

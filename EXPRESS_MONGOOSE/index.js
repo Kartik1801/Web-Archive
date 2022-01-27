c81a309980947a009dcbfed447260c33fb2615fc
kartik1801 <kartikdhawan1801@gmail.com>
2022-01-28 01:38:59 +0530

    Using Express with mongoose

--- EXPRESS_MONGOOSE/index.js
+++ EXPRESS_MONGOOSE/index.js
@@ -1,10 +1,11 @@
 // IIFE - Style :)  
-((express, app, path, dotenv, mongoose, Product) => {   
+((express, app, path, dotenv, mongoose, Product, methodOverride) => {   
     mongoose.connect(`mongodb://localhost:27017/vendor`)
      .then(() => console.log("Connected to mongo!!!"))
      .catch( err => console.log("Error",err))
 
     app.use(express.urlencoded({extended:true}))
+    app.use(methodOverride("_method"))
     app.set('views', path.join(__dirname, 'views'));
     app.set('view engine', 'ejs');
 
@@ -13,35 +14,50 @@
         const products = await Product.find({});
         res.render('products/index', {products: products});
     })
-
+    // Add a product
+    app.get("/products/new", async (req, res) => {
+        const category = await Product.find({}).distinct("category");
+        res.render("products/new",{categories: category});
+    })
+    
+    app.post('/products',async (req, res) => {
+            const newProduct = Product(req.body);
+            await newProduct.save();
+            console.log(newProduct);
+            res.redirect('/products');
+        })
+        
+    
+    // Edit a product
+    app.get('/products/:id/edit', async (req, res) => {
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        const category = await Product.find({}).distinct("category");
+        res.render("products/edit",{product, categories: category});
+    })
+    app.put('/products/:id', async (req, res) => {
+        const {id} = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true });
+        res.redirect(`/products/${product._id}`);
+    })
+    
+    app.delete('/products/:id', async (req, res) => {
+        const {id} = req.params;
+        const product = await Product.findByIdAndRemove(id);
+        res.redirect(`/products`);
+    })
+    
     // Show Product Details
     app.get('/products/:id', async (req, res) => {
         const { id } = req.params;
         const product = await Product.findById(id);
         res.render("products/details",{product});
     })
-
-    // Add a product
-    app.get('/products/new', (req, res) => {
-        res.render('products/new')
-    })
-    app.post('/products',async (req, res) => {
-        const newProduct = Product(req.body);
-        await newProduct.save();
-        console.log(newProduct);
-        res.redirect('/products');
-    })
-
-    app.get('/products/:id/edit', async (req, res) => {
-        const { id } = req.params;
-        const product = await Product.findById(id);
-        res.render("products/edit",{product});
-    })
-
+    
     app.listen( process.env.PORT, ()=>{
         console.log('Listening on port:',process.env.PORT);
     })
-
+    
 })
 (
     require(`express`),
@@ -49,5 +65,6 @@
     require(`path`),
     require(`dotenv`).config(),
     require(`mongoose`),
-    require(`./models/product`)
+    require(`./models/product`),
+    require(`method-override`)
 )

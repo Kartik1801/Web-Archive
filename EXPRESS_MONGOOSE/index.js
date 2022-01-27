0d8e3a1c26dfbd1893c92051b6131c6b2d4aa540
kartik1801 <kartikdhawan1801@gmail.com>
2022-01-27 22:25:44 +0530

    Using Express with mongoose

--- EXPRESS_MONGOOSE/index.js
+++ EXPRESS_MONGOOSE/index.js
@@ -1,15 +1,16 @@
 // IIFE - Style :)  
-((express, app, path, dotenv, mongoose) => {
-    
-    mongoose.connect('mongodb://localhost:27017/')
+((express, app, path, dotenv, mongoose, Product) => {
+   
+    mongoose.connect(`mongodb://localhost:27017/vendor`)
      .then(() => console.log("Connected to mongo!!!"))
      .catch( err => console.log("Error",err))
 
     app.set('views', path.join(__dirname, 'views'));
     app.set('view engine', 'ejs');
 
-    app.get(`/dogs`,(req, res) => {
-        res.send("WooF")       
+    app.get(`/products`,async (req, res) => {
+        const products = await Product.find({});
+        res.render('products/index', {products: products});
     })
     
     app.listen( process.env.PORT, ()=>{
@@ -18,9 +19,10 @@
 
 })
 (
-    require('express'),
-    require('express')(),
-    require('path'),
-    require('dotenv').config(),
-    require('mongoose')
+    require(`express`),
+    require(`express`)(),
+    require(`path`),
+    require(`dotenv`).config(),
+    require(`mongoose`),
+    require(`./models/product`)
 )
--- EXPRESS_MONGOOSE/models/product.js
+++ EXPRESS_MONGOOSE/models/product.js
@@ -1,9 +1,24 @@
 ((mongoose) => {   
-    mongoose.connect('mongodb://localhost:27017/')
-     .then( () => console.log("Connected to mongo!!!"))
-     .catch( err => console.log("Error",err))
 
-     const productSchema = new Mongoose.Schema
+    const productSchema = new mongoose.Schema({
+         name: {
+             type : String,
+             required: true
+         },
+         price: {
+             type: Number,
+             required: true,
+             min : 0
+         }, 
+         category: {
+             type: String,
+             lowercase: true,
+             enum: [ 'fruit', 'vegetable', 'dairy']
+         }
+     });
+
+     const Product = mongoose.model('Product', productSchema);
+     module.exports = Product;
 })
 (
     require('mongoose')

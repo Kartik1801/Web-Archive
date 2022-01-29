c32901e627729a929eaaf0f07b7627c7689b83fd
kartik1801 <kartikdhawan1801@gmail.com>
2022-01-29 18:29:00 +0530

    Using middleware with Express

--- Middleware_Intro/index.js
+++ Middleware_Intro/index.js
@@ -2,19 +2,22 @@ const express = require('express');
 const app = express();
 const morgan =require("morgan");
 
+var requestTime = (req, res, next) => {
+    req.requestTime = Date.now();
+    next();
+}
+
 app.use(morgan("dev"))
-app.use(async (req, res, next) => {
-    console.log("Hijacking")
-    return next();
-    console.log("Hijacked")
-})
+app.use(requestTime)
 
 app.get("/",(req,res) => {
     console.log("home");
-    res.send("Home");
+    let response = `${req.requestTime}`
+    res.send(response + "<br> Home");
 });
 app.get("/dog",(req,res) => {
     console.log("Woofy");
-    res.send("woof woof");
+    let response = `${req.requestTime}`
+    res.send(response + "<br> woof woof");
 });
 app.listen(3000,() => {console.log('listening on 3000')});

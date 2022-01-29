21b96a795597f8757b95d43ebcfa4a4d607faf6c
kartik1801 <kartikdhawan1801@gmail.com>
2022-01-29 18:01:21 +0530

    Using middlewares

--- Middleware_Intro/index.js
+++ Middleware_Intro/index.js
@@ -2,15 +2,19 @@ const express = require('express');
 const app = express();
 const morgan =require("morgan");
 
-app.use(express.urlencoded({extended:true}))
-app.use(morgan("combined"))
-app.use(morgan("common"))
 app.use(morgan("dev"))
+app.use(async (req, res, next) => {
+    console.log("Hijacking")
+    return next();
+    console.log("Hijacked")
+})
 
 app.get("/",(req,res) => {
+    console.log("home");
     res.send("Home");
 });
 app.get("/dog",(req,res) => {
+    console.log("Woofy");
     res.send("woof woof");
 });
 app.listen(3000,() => {console.log('listening on 3000')});

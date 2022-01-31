25e102fe6faf9ae85ac8c74484c1c8babac5dcf6
kartik1801 <kartikdhawan1801@gmail.com>
2022-02-01 00:16:47 +0530

    Handling Async Errors in Express

--- Handling_Async_Errors_in_Express/index.js
+++ Handling_Async_Errors_in_Express/index.js
@@ -4,24 +4,27 @@
     mongoose.connect(`mongodb://localhost:27017/vendors2`)
      .then(() => console.log("Connected to mongo!!!"))
      .catch( err => console.log("Error",err))
-
     // Express middlewares
     app.use(express.urlencoded({extended:true}))
     app.use(methodOverride("_method"))
     app.set('views', path.join(__dirname, 'views'));
     app.set('view engine', 'ejs');
-    
-    // Routes: 
+    // Routes:
+    // 
+    app.get('/',(req,res) => {
+        res.send("Go to /products")
+    }) 
     // Show All Products
-    app.get(`/products`,async (req, res) => {
+    app.get(`/products`,async (req, res, next) => {
         const { category } = req.query;
+        if (!category) return next(new generateError(404,"No Category Found"));
         const products =category?await Product.find({category}):await Product.find({});
         res.render('products/index', {products: products, category : category?category:"All"});
     })
-    
     // Add a product
     app.get("/products/new", async (req, res) => {
         const category = await Product.find({}).distinct("category");
+        if (!category) return next(new generateError(404,"No Category Found"));
         res.render("products/new",{categories: category});
     })
     app.post('/products',async (req, res) => {
@@ -29,41 +32,40 @@
             await newProduct.save();
             res.redirect('/products');
         })
-    
     // Edit a product
     app.get('/products/:id/edit', async (req, res) => {
         const { id } = req.params;
         const product = await Product.findById(id);
+        if (!product) return next(new generateError(404,"No Product Found"));
         const category = await Product.find({}).distinct("category");
+        if (!category) return next(new generateError(404,"No Category Found"));
         res.render("products/edit",{product, categories: category});
     })
     app.put('/products/:id', async (req, res) => {
         const { id } = req.params;
         const product = await Product.findByIdAndUpdate(id, req.body, {runValidators: true, new: true });
+        if (!product) return next(new generateError(404,"No Product Found"));
         res.redirect(`/products/${product._id}`);
     })
-
     // Remove a product
     app.delete('/products/:id', async (req, res) => {
         const { id } = req.params;
         const product = await Product.findByIdAndRemove(id);
+        if (!product) return next(new generateError(404,"No Product Found"));
         res.redirect(`/products`);
     })
-
     // Show Product Details
     app.get('/products/:id', async (req, res) => {
         const { id } = req.params;
         const product = await Product.findById(id);
+        if (!product) return next(new generateError(404,"No Product Found"));
         res.render("products/details", {product});
     })
-
      // Custom Error Handler:
     app.use((err, req, res, next) => {
-        const {status = 500} = err;
-        const {message = "Something Went Wrong!!!"} = err;
+        const {status = 500, message = "Something Went Wrong!!!"} = err;
         res.status(status).send(message);
     });
-
          
     app.listen(process.env.PORT, () => {
         console.log('Listening on port:', process.env.PORT);

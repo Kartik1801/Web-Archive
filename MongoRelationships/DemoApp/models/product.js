0ec76c1a30b7299a146c97b9661e7741560af9e2
kartik1801 <kartikdhawan1801@gmail.com>
2022-02-02 23:44:25 +0530

    Initial Commit : Mongo Relations

---
+++ MongoRelationships/DemoApp/models/product.js
@@ -0,0 +1,25 @@
+((mongoose) => {   
+
+    const productSchema = new mongoose.Schema({
+         name: {
+             type : String,
+             required: true
+         },
+         price: {
+             type: Number,
+             required: true,
+             min : 0
+         }, 
+         category: {
+             type: String,
+             lowercase: true,
+             enum: [ 'fruit', 'vegetable', 'dairy']
+         }
+     });
+
+     const Product = mongoose.model('Product', productSchema);
+     module.exports = Product;
+})
+(
+    require('mongoose')
+)

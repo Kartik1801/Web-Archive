e18a765d3a28bd90bc526c4a303ca723a4dbb158
kartik1801 <kartikdhawan1801@gmail.com>
2022-02-04 15:42:43 +0530

    Cookies Signing

--- Cookies_Demo/index.js
+++ Cookies_Demo/index.js
@@ -2,11 +2,21 @@ const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
 
-app.use(cookieParser())
+app.use(cookieParser('Secret'))
 
 app.get("/user", (req, res) => {
     console.log(req.cookies)
     res.send(`HOLA ${req.cookies.name}`)
+})
+
+app.get('/getsignedcookie', (req, res) => {
+    res.cookie("KEY", "value", { signed: true });
+    res.send("Signed")
+})
+
+app.get('/verifysign', (req, res) => {
+    console.log(req.cookies)
+    res.send(req.signedCookies)
 })
 
 app.get('/setName',(req, res) =>{

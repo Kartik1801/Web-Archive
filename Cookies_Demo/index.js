6a60b6a36336143f944e56b6bccd7718725f7b9e
kartik1801 <kartikdhawan1801@gmail.com>
2022-02-05 23:39:36 +0530

    COOKIES

--- Cookies_Demo/index.js
+++ Cookies_Demo/index.js
@@ -1,32 +1,25 @@
 const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
-
 // HMAC SIGNING COOKIES: LOOK INTO PACKAGE COOKIE PARSER FOR DETAILSs 
 // Signing cookies verify wether the cookie has been tempered with
-
 app.use(cookieParser('Secret'))
-
 app.get("/user", (req, res) => {
     console.log(req.cookies)
     res.send(`HOLA ${req.cookies.name}`)
 })
-
 app.get('/getsignedcookie', (req, res) => {
     res.cookie("KEY", "value", { signed: true });
     res.send("Signed")
 })
-
 app.get('/verifysign', (req, res) => {
     console.log(req.cookies)
     res.send(req.signedCookies)
 })
-
 app.get('/setName',(req, res) =>{
     res.cookie('name', "KD");
     res.send("Sent you a cookie")
 })
-
 app.listen(3000, () => {
     console.log("Listening on port: 3000");
 })

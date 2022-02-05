b369580885232e4cb2b7952127069a5aad8e77a8
kartik1801 <kartikdhawan1801@gmail.com>
2022-02-05 23:39:43 +0530

    Using Express Routers

--- EXPRESS_ROUTER_BASICS/index.js
+++ EXPRESS_ROUTER_BASICS/index.js
@@ -2,9 +2,7 @@
 const express = require('express');
 const app = express();
 const shelterRoutes = require("./routes/shelters");
-
 app.use("/shelters", shelterRoutes);
-
 app.listen(3000, () => {
     console.log("Listening on port: 3000");
 })

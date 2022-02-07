41d414d110d1a7ccbecfa4c1bda184e9c07fb611
kartik1801 <kartikdhawan1801@gmail.com>
2022-02-08 00:21:20 +0530

    Basic Login Auth

--- AuthDemo/index.js
+++ AuthDemo/index.js
@@ -1,11 +1,46 @@
 const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose')
 const app = express();
 const model = require('./models/user');
+const bcrypt = require('bcrypt')
+
+mongoose.connect("mongodb://localhost:27017/BasicAuth")
+.then(() => console.log('MongoDB connected'))
+.catch(() => console.log('Error connecting to MongoDB'))
 
 app.set("view engine", "ejs")
 app.set('views', path.join(__dirname, 'views'))
+app.use(express.urlencoded({extended:true}))
+
+app.get("/", (req, res) => {
+    res.send("HOMEPAGE")
+})
+
 app.get("/register", (req, res) => {
     res.render("register")
+})
+app.post("/register", async (req, res) => {
+    const {username, password} = req.body
+    const hash = await bcrypt.hash(password, 12);
+    const user = new model({
+        username: username,
+        password: hash
+    })
+    await user.save()
+    res.redirect("/");
+})
+
+app.get("/login", (req, res) => {
+    res.render("login")
+})
+app.post("/login", async (req, res) => {
+    const {username, password} = req.body
+    const user = await model.findOne({ username });
+    if(await bcrypt.compare(password, user.password)){
+        res.redirect("/access");
+    }
+    else res.send("INVALID PASSWORD / USERNAME")
 })
 
 app.get("/access", (req, res) => {
